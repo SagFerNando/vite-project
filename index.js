08af63799904c570b8a4832d3d7f99679e7bd4fc
SagFerNando <111102672+SagFerNando@users.noreply.github.com>
2023-10-05 13:47:13 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import { hello } from "./src/helloWorld.js";
+
+console.clear()
+
+console.log(hello());
+
+// const arreglo=[8,9,5,2];
+// const objeto={
+//     name:'AVM',
+//     tipo: 'Transporte',
+//     camion :{
+//         name:'5420'
+//     },
+// };
+// //imprimir una propiedd del objeyo y luego todo el objeto 
+// console.log(objeto['name']);
+// console.log(objeto);
+
+// const fer2 = () => {
+//     console.log('helou word');
+// };
+
+// fer2();
+
+
+const test = true;
+//test ? console.log('hola'):
+console.log(`fer ${test}`)
+
